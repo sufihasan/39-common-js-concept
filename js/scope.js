c0bceb5b807bc13dbbdf7f63b528d75f31c5b42d
sufihasan <hasan6nh@gmail.com>
2022-12-10 18:41:13 +0600

    make scope

---
+++ js/scope.js
@@ -0,0 +1,17 @@
+function totalBlance(a, b) {
+
+    const total = a + b;
+    console.log(total);
+
+    if (b > 5) {
+
+        const sum = a + b;
+        // console.log(sum);
+    }
+
+}
+
+console.log(sum);
+// console.log(total);
+
+totalBlance(20, 30);
---
+++ js/triple.js
@@ -0,0 +1,15 @@
+const a = 20;
+const b = '20';
+
+if (a == b) {
+    console.log('both are equal');
+
+}
+
+
+const d = 20;
+const e = '20';
+
+if (d === e) {
+    // console.log('both are equal');
+}
